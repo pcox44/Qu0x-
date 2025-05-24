4b586193ebcd97e8c4010d6deb91fc185afd3b30
pcox44 <cox.patrick.c@gmail.com>
2025-05-23 22:42:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -82,7 +82,25 @@ function styleDie(die, val) {
 }
 
 function addToExpression(char) {
-  expressionBox.innerText += char;
+  const expr = expressionBox.innerText;
+  const lastChar = expr.slice(-1);
+
+  // Define what counts as a number character (digits)
+  const isDigit = c => /\d/.test(c);
+
+  // If char is a digit (from dice):
+  if (isDigit(char)) {
+    // If last char is also a digit, add a space before adding new digit to prevent concatenation
+    if (isDigit(lastChar)) {
+      expressionBox.innerText += ' ' + char;
+    } else {
+      expressionBox.innerText += char;
+    }
+  } else {
+    // For operators and parentheses, append directly
+    expressionBox.innerText += char;
+  }
+
   evaluateExpression();
 }
 
